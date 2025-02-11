c6d95e07fbc0387477216c1d9a940ca6c5869645
Johnny <phamj1998@gmail.com>
2025-02-11 10:05:17 -0600

    tweaked player script to get more familiar with syntax and added sprite flip code when player moves left/right

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,15 +11,30 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("z_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	# ------------------------------------------------------
+	if Input.is_key_pressed(KEY_SHIFT):
+		print("Shift key pressed!")
+	print(direction)
+	# ------------------------------------------------------
+	
 	if direction:
+		# -------------------------------------
+		if Input.is_key_pressed(KEY_LEFT):
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
+		print("with direction:", velocity.x)
+		# -------------------------------------
 		velocity.x = direction * SPEED
 	else:
+		print("no direction:", velocity.x)
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
